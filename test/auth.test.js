5966eca06ffadf6dc017879602e84743b89a0369
Mihai Dima <mihaizn@gmail.com>
2015-12-16 14:01:44 +0200

    linting, update package.json

--- test/auth.test.js
+++ test/auth.test.js
@@ -1,49 +1,51 @@
-"use strict";
+'use strict'
 
 var assert = require('assert')
-var seneca = require('seneca')
-var Cookies = require('cookies')
+var Seneca = require('seneca')
+var Lab = require('lab')
 
-var si = seneca()
-si.use( 'user' )
-si.use( require('..'),{secure:true} )
+var lab = exports.lab = Lab.script()
+var describe = lab.describe
+var it = lab.it
 
-var useract = si.pin({role:'user',cmd:'*'})
-var authact = si.pin({role:'auth',cmd:'*'})
+var si = Seneca()
+si.use('user')
+si.use(require('..'), {secure: true})
 
+var authact = si.pin({role: 'auth', cmd: '*'})
 
-describe('auth', function() {
+describe('auth', function () {
+  it('happy', function (cb) {
+    authact.register({data: {nick: 'u1', name: 'u1n', password: 'u1p'}}, function (err, out) {
+      if (err) return cb(err)
 
-  it('happy', function(cb) {
-
-    authact.register({data:{nick:'u1',name:'u1n',password:'u1p'}},function(err,out){
-      if( err ) return cb(err);
-      
       assert.ok(out.ok)
       assert.ok(out.user)
       assert.ok(out.login)
       cb()
     })
-
   })
 
-  it('use secure option', function(cb) {
-
+  it('use secure option', function (cb) {
     var res = {}
     var req = {}
 
-    si.cookies = { set: function(tokenkey,id,options){
-      assert.ok(options.secure)
+    si.cookies = {
+      set: function (tokenkey, id, options) {
+        assert.ok(options.secure)
 
-      cb()
-    }}
+        cb()
+      }
+    }
     res.seneca = si
     req.seneca = si
-    authact.login({user:{nick:'u1',name:'u1n',password:'u1p'},auto:true,req$:req,res$:res},function(err,out){
-      if( err ) return cb(err);
-
-
+    authact.login({
+      user: {nick: 'u1', name: 'u1n', password: 'u1p'},
+      auto: true,
+      req$: req,
+      res$: res
+    }, function (err, out) {
+      if (err) return cb(err)
     })
-
   })
 })
